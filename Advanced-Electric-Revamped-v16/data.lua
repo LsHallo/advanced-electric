39789df4c9d50ff7216fba94e89450951b6966ae
LsHallo <xlshallox@gmail.com>
2020-08-28 23:34:26 +0200

    add settings for different tiers

--- Advanced-Electric-Revamped-v16/data.lua
+++ Advanced-Electric-Revamped-v16/data.lua
@@ -1,33 +1,49 @@
- -- advanced accumulator
+enable_advanced=settings.startup["advanced-electric-enable-advanced"].value
+enable_elite=settings.startup["advanced-electric-enable-elite"].value
+enable_ultimate=settings.startup["advanced-electric-enable-ultimate"].value
+
+-- advanced accumulator
 require("prototypes.entity.advanced-accumulator")
 require("prototypes.item.advanced-accumulator")
-require("prototypes.crafting.advanced-accumulator")
-require("prototypes.technology.advanced-accumulator")
- -- elite accumulator
-require("prototypes.entity.elite-accumulator")
-require("prototypes.item.elite-accumulator")
-require("prototypes.crafting.elite-accumulator")
-require("prototypes.technology.elite-accumulator")
- -- ultimate accumulator
-require("prototypes.entity.ultimate-accumulator")
-require("prototypes.item.ultimate-accumulator")
-require("prototypes.crafting.ultimate-accumulator")
-require("prototypes.technology.ultimate-accumulator")
- -- advanced solar
+if enable_advanced then
+	require("prototypes.crafting.advanced-accumulator")
+	require("prototypes.technology.advanced-accumulator")
+end
+-- advanced solar
 require("prototypes.entity.advanced-solar")
 require("prototypes.item.advanced-solar")
-require("prototypes.crafting.advanced-solar")
-require("prototypes.technology.advanced-solar")
- -- elite solar
+if enable_advanced then
+	require("prototypes.crafting.advanced-solar")
+	require("prototypes.technology.advanced-solar")
+end
+-- elite accumulator
+require("prototypes.entity.elite-accumulator")
+require("prototypes.item.elite-accumulator")
+if enable_elite and enable_advanced then
+	require("prototypes.crafting.elite-accumulator")
+	require("prototypes.technology.elite-accumulator")
+end
+-- elite solar
 require("prototypes.entity.elite-solar")
 require("prototypes.item.elite-solar")
-require("prototypes.crafting.elite-solar")
-require("prototypes.technology.elite-solar")
- -- ultimate solar
+if enable_elite and enable_advanced then
+	require("prototypes.crafting.elite-solar")
+	require("prototypes.technology.elite-solar")
+end
+-- ultimate accumulator
+require("prototypes.entity.ultimate-accumulator")
+require("prototypes.item.ultimate-accumulator")
+if enable_ultimate and enable_elite and enable_advanced then
+	require("prototypes.crafting.ultimate-accumulator")
+	require("prototypes.technology.ultimate-accumulator")
+end
+-- ultimate solar
 require("prototypes.entity.ultimate-solar")
 require("prototypes.item.ultimate-solar")
-require("prototypes.crafting.ultimate-solar")
-require("prototypes.technology.ultimate-solar")
+if enable_ultimate and enable_elite and enable_advanced then
+	require("prototypes.crafting.ultimate-solar")
+	require("prototypes.technology.ultimate-solar")
+end
 
 
 data.raw["accumulator"]["accumulator"].fast_replaceable_group = "accumulator"
---
+++ Advanced-Electric-Revamped-v16/settings.lua
@@ -0,0 +1,20 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "advanced-electric-enable-advanced",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "advanced-electric-enable-elite",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "advanced-electric-enable-ultimate",
+        setting_type = "startup",
+        default_value = true
+    },
+})
